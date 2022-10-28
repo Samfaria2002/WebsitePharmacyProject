02e43bb0058905ddc594c76afac8757b98ab5a5c
Spartt <leoguedes3011@gmail.com>
2022-10-27 22:03:52 -0300

    adding context to execute db.create_all()

--- mysite/flask_app.py
+++ mysite/flask_app.py
@@ -35,5 +35,8 @@ assets.register('main_scss', main)
 app.register_blueprint(views)
 
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+    
     app.run()
     #app.run(host = "0.0.0.0", port = 5000, debug = True)
